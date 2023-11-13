0f1b78c92bc5ef5e6a1fece3d37ab5d36de7d7fe
Ali <anwar.ali75751@yahoo.fr>
2023-11-13 16:26:30 +0100

    prototype version

--- exo1.js
+++ exo1.js
@@ -7,6 +7,23 @@ function myMap(arr, func) {
   return newArr;
 }
 
+Object.defineProperty(Array.prototype, "myMap", {
+  value: function (func) {
+    const newArr = [];
+    for (let i = 0; i < this.length; i++) {
+      const result = func(this[i]);
+      newArr.push(result);
+    }
+    return newArr;
+  },
+});
+
+
 // test
-console.log("Test exo1 :");
+console.log("Exo1.");
+console.log("Test myMap() :");
 console.log(myMap([0, 1, 7, 33], val => val * 3)); // [0, 3, 21, 99]
+
+// test prototype
+console.log("Test prototype :");
+console.log([0, 4, 7, 33].myMap(val => val * 3)); // [0, 12, 21, 99]
