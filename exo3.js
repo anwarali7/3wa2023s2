faf56e6b428a07fd8d9dfbec8c8ca6437340dba1
Ali <anwar.ali75751@yahoo.fr>
2023-11-13 11:38:25 +0100

    exo3

---
+++ exo3.js
@@ -0,0 +1,12 @@
+function myReduce(arr, func, initialValue) {
+  let acc = initialValue === undefined ? 0 : initialValue;
+  for (let i = 0; i < arr.length; i++) {
+    acc = func(acc, arr[i]);
+  }
+  return acc;
+}
+
+// test
+console.log("Test exo3 :");
+console.log(myReduce([1,2,3,4], (accumulator, currentValue) => 
+  accumulator + currentValue, 0)); // Expected output: 10
